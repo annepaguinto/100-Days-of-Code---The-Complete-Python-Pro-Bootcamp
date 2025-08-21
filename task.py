20767c86562c0112d2c7a8458055c3fed40f783b
Rose Anne Paguinto <145623280+annepaguinto@users.noreply.github.com>
2025-08-21 11:37:10 +0800

    Day 1 project: Band Name Generator
    Here are the things I learned to complete the Band Name Generator project
    - Downloaded and set up PyCharm
    - Completed a printing and debugging practice
    - Learned about String manipulation, Code Intelligence and Variable Naming
    - Introduced to Python Variables

---
+++ task.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city = input("What city did you grew up in? ")
+pet = input("What is the name of your pet? ")
+band_name = city + " " + pet
+print("Your band name is " + band_name)
