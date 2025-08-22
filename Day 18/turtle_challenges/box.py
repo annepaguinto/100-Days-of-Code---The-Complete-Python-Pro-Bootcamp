f8d13b464b92eb73da697e9a63133dfc9aa56497
Rose Anne Paguinto <145623280+annepaguinto@users.noreply.github.com>
2025-08-22 09:09:08 +0800

    Create box.py

---
+++ Day 18/turtle_challenges/box.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+timmy.shape("arrow")
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+
+
+timmy = Screen()
+timmy.exitonclick()
